d0e52c56662b058acd281c383e39c5fbe3e2a996
tanvir244 <tr30365s25@gmail.com>
2024-04-09 11:06:11 +0600

    firebase deployment complete

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDarG9anlsTE8jv_ABlsBxqkKVntBrwRCs",
-  authDomain: "react-dragon-news-firebase-au.firebaseapp.com",
-  projectId: "react-dragon-news-firebase-au",
-  storageBucket: "react-dragon-news-firebase-au.appspot.com",
-  messagingSenderId: "888525060555",
-  appId: "1:888525060555:web:a6f7d4c699dd04bcff9e4e"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
